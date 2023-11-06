6693625c701dc835daf791a50c401f81f6b02930
yoyo30618 <yoyo30618@gmail.com>
2023-11-06 21:05:51 +0800

    修正連線字串

--- conn_mysql.php
+++ conn_mysql.php
@@ -1,10 +1,13 @@
 <?php
-	$db_link=@mysqli_connect("127.0.0.1","root","yoyo0516");
+	$Env="Local" //Server Local
+	if($Env=="Server") $db_link=@mysqli_connect("127.0.0.1","yoyo30618","yoyo0516");
+	else $db_link=@mysqli_connect("127.0.0.1","root","yoyo0516");
 	if(!$db_link)
 		die("資料庫連線失敗<br>");
 	else{
 		mysqli_query($db_link, 'SET NAMES utf8');
-		$seldb=@mysqli_select_db($db_link,"TELic-LAB");
+		if($Env=="Server") $seldb=@mysqli_select_db($db_link,"BearTest");
+		else $seldb=@mysqli_select_db($db_link,"TELic-LAB");
 		if(!$seldb)
 			die("資料庫選擇失敗<br>");
 	}
